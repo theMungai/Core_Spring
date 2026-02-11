6980b1b32f4932e7f9b0675b0c22a3e101f13741
theMungai <mungaithesimon@gmail.com>
2026-02-11 09:14:46 +0300

    Inject objects using Spring

---
+++ 03-Injecting-objects/src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        // Triangle triangle = new Triangle();
+
+        //BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        Triangle triangle = (Triangle) context.getBean("triangle");
+
+        triangle.draw();
+
+    }
+}
