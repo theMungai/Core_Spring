e0b746c49f99019a81904e04da58e4ef429e4df1
theMungai <mungaithesimon@gmail.com>
2026-02-12 12:16:09 +0300

    Learn Bean Autowiring and also tackle ApplicationContextAware to get access of the Spring container (ApplicationContext).

---
+++ 05-Bean-Autowiring/src/main/java/org/theMungai/Main.java
@@ -0,0 +1,14 @@
+package org.theMungai;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        Triangle triangle = (Triangle) context.getBean("triangle");
+
+        triangle.draw();
+    }
+}
