110e71e3ff39868f5292584057a869901342183e
theMungai <mungaithesimon@gmail.com>
2026-02-12 17:00:43 +0300

    Bean definition inheritance on lists

--- 06-Bean-definition-inheritance/src/main/java/org/theMungai/Triangle.java
+++ 06-Bean-definition-inheritance/src/main/java/org/theMungai/Triangle.java
@@ -1,56 +1,26 @@
 package org.theMungai;
 
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.BeanNameAware;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 
+import java.util.List;
 
-public class Triangle implements ApplicationContextAware, BeanNameAware {
+public class Triangle {
 
-    private Point pointA;
-    private Point pointB;
-    private Point pointC;
-    private ApplicationContext context = null;
-
-    public Point getPointA() {
-        return pointA;
+    public List<Point> getPoints() {
+        return points;
     }
 
-    public void setPointA(Point pointA) {
-        this.pointA = pointA;
+    public void setPoints(List<Point> points) {
+        this.points = points;
     }
 
-    public Point getPointB() {
-        return pointB;
-    }
+    private List<Point> points;
 
-    public void setPointB(Point pointB) {
-        this.pointB = pointB;
-    }
-
-    public Point getPointC() {
-        return pointC;
-    }
-
-    public void setPointC(Point pointC) {
-        this.pointC = pointC;
-    }
 
 
     public void draw(){
-        System.out.println("Point A = (" + getPointA().getX() + ", " + getPointA().getY() + ")");
-        System.out.println("Point B = (" + getPointB().getX() + ", " + getPointB().getY() + ")");
-        System.out.println("Point C = (" + getPointC().getX() + ", " + getPointC().getY() + ")");
+        for (Point point : points){
+            System.out.println("Point = (" + point.getX() + ", " + point.getY() + ")");
+        }
     }
 
-    @Override
-    public void setApplicationContext(ApplicationContext context) throws BeansException {
-        this.context = context;
-    }
-
-    @Override
-    public void setBeanName(String beanName) {
-        System.out.println("Bean name is: " + beanName);
-    }
 }
